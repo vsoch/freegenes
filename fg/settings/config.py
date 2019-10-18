c38732ec0c0ad974104c4d6a4d4409f103020993
Keoni Gandall <koeng101@gmail.com>
2019-10-18 14:35:57 -0700

    Added convert2boolean for users who dont read docs

--- fg/settings/config.py
+++ fg/settings/config.py
@@ -8,44 +8,41 @@ with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 '''
 import os
-from scif.logger import bot
 
 def getenv(variable_key, default=None, required=False, silent=True, boolean=False):
     ''' getenv will attempt to get an environmental variable. If the variable
         is not found, the deafult value will be returned.
-
         Parameters
         ==========
         variable_key: the variable name to be searched for
         required: exit with error if not found
         silent: if silent, does not print debugging information
-
         All credit to https://github.com/vsoch/scif/blob/master/scif/defaults.py
     '''
     variable = os.environ.get(variable_key)
-    if variable is None and required == True:
-        bot.exit("Cannot find environment variable {}, exiting.".format(variable_key))
+    if variable is None and required:
+        raise ValueError("Cannot find environment variable {}, exiting.".format(variable_key)) # replace bot
 
-    if silent != True and variable is not None:
-        bot.verbose("{} found as {}".format(variable_key,variable))
+    if boolean and variable is not None:
+        if not isinstance(variable, bool):
+            variable = variable.lower() in ("yes", "true", "t", "1", "y")
 
-    if boolean == True:
-        variable = variable == 'True' # Handles booleans
+    if not silent and variable is not None:
+        print("{} found as {}".format(variable_key, variable)) # Replaced bot
 
     return variable
-
 
 # AUTHENTICATION
 
 # Which social auths do you want to use?
-ENABLE_GLOBUS_AUTH = getenv('FG_ENABLE_GLOBUS_AUTH',default=False,boolean=True)
-ENABLE_GOOGLE_AUTH = getenv('FG_ENABLE_GOOGLE_AUTH',default=False,boolean=True)
-ENABLE_ORCID_AUTH = getenv('FG_ENABLE_ORCID_AUTH',default=False,boolean=True)
-ENABLE_ORCID_AUTH_SANDBOX = getenv('FG_ENABLE_ORCID_AUTH_SANDBOX',default=False,boolean=True)
-ENABLE_TWITTER_AUTH = getenv('FG_ENABLE_TWITTER_AUTH',default=False,boolean=True)
-ENABLE_GITHUB_AUTH = getenv('FG_ENABLE_GITHUB_AUTH',default=True,boolean=True)
-ENABLE_GITLAB_AUTH = getenv('FG_ENABLE_GITLAB_AUTH',default=False,boolean=True)
-ENABLE_BITBUCKET_AUTH = getenv('FG_ENABLE_BITBUCKET_AUTH',default=False,boolean=True)
+ENABLE_GLOBUS_AUTH = getenv('FG_ENABLE_GLOBUS_AUTH', default=False,boolean=True)
+ENABLE_GOOGLE_AUTH = getenv('FG_ENABLE_GOOGLE_AUTH', default=False,boolean=True)
+ENABLE_ORCID_AUTH = getenv('FG_ENABLE_ORCID_AUTH', default=False,boolean=True)
+ENABLE_ORCID_AUTH_SANDBOX = getenv('FG_ENABLE_ORCID_AUTH_SANDBOX', default=False,boolean=True)
+ENABLE_TWITTER_AUTH = getenv('FG_ENABLE_TWITTER_AUTH', default=False,boolean=True)
+ENABLE_GITHUB_AUTH = getenv('FG_ENABLE_GITHUB_AUTH', default=True,boolean=True)
+ENABLE_GITLAB_AUTH = getenv('FG_ENABLE_GITLAB_AUTH', default=False,boolean=True)
+ENABLE_BITBUCKET_AUTH = getenv('FG_ENABLE_BITBUCKET_AUTH', default=False,boolean=True)
 
 # NOTE you will need to set authentication methods up.
 # Configuration goes into secrets.py
@@ -56,32 +53,32 @@ ENABLE_BITBUCKET_AUTH = getenv('FG_ENABLE_BITBUCKET_AUTH',default=False,boolean=
 
 # Shipping (set this variable in your secrets.py
 SHIPPO_TOKEN=getenv('FG_SHIPPO_TOKEN',default=None)
-SHIPPO_CUSTOMER_REFERENCE=getenv('FG_SHIPPO_CUSTOMER_REFERENCE',default=None)
-GOOGLE_ANALYTICS_ID=getenv('FG_GOOGLE_ANALYTICS_ID',default=None)
+SHIPPO_CUSTOMER_REFERENCE=getenv('FG_SHIPPO_CUSTOMER_REFERENCE', default=None)
+GOOGLE_ANALYTICS_ID=getenv('FG_GOOGLE_ANALYTICS_ID', default=None)
 
 # SendGrid
 
-SENDGRID_API_KEY=getenv('FG_SENDGRID_API_KEY',default=None)
+SENDGRID_API_KEY=getenv('FG_SENDGRID_API_KEY', default=None)
 
 # DOMAIN NAMES
 ## IMPORTANT: if/when you switch to https, you need to change "DOMAIN_NAME"
 # to have https, otherwise some functionality will not work
 
-DOMAIN_NAME = getenv('FG_DOMAIN_NAME',default="http://127.0.0.1")
+DOMAIN_NAME = getenv('FG_DOMAIN_NAME', default="http://127.0.0.1")
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = DOMAIN_NAME
 
-ADMINS = ((getenv('FG_ADMIN_NAME',default='vsochat'), getenv('FG_ADMIN_EMAIL',default='vsochat@gmail.com'),)
+ADMINS = ((getenv('FG_ADMIN_NAME', default='vsochat'), getenv('FG_ADMIN_EMAIL', default='vsochat@gmail.com'),)
 MANAGERS = ADMINS
 
 # BioNode Parameters
 
-HELP_CONTACT_EMAIL = getenv('FG_HELP_CONTACT_EMAIL',default='vsochat@stanford.edu')
-HELP_CONTACT_PHONE = getenv('FG_HELP_CONTACT_PHONE',default=None) # "123-456-7890"
+HELP_CONTACT_EMAIL = getenv('FG_HELP_CONTACT_EMAIL', default='vsochat@stanford.edu')
+HELP_CONTACT_PHONE = getenv('FG_HELP_CONTACT_PHONE', default=None) # "123-456-7890"
 HELP_INSTITUTION_SITE = getenv('FG_HELP_INSTITUTION_SITE', default='https://srcc.stanford.edu')
-NODE_INSTITUTION = getenv('FG_NODE_INSTITUTION',default='Stanford University')
-NODE_URI = getenv('FG_NODE_URI',default="freegenes")
-NODE_NAME = getenv('FG_NODE_NAME',default="FreeGenes")
-NODE_TWITTER = getenv('FG_NODE_TWITTER',default="biobricks")
+NODE_INSTITUTION = getenv('FG_NODE_INSTITUTION', default='Stanford University')
+NODE_URI = getenv('FG_NODE_URI', default="freegenes")
+NODE_NAME = getenv('FG_NODE_NAME', default="FreeGenes")
+NODE_TWITTER = getenv('FG_NODE_TWITTER', default="biobricks")
 
 # Defaults for Physical Models
 
@@ -89,8 +86,8 @@ NODE_TWITTER = getenv('FG_NODE_TWITTER',default="biobricks")
 # (addresses) for the well schema.
 # Question: is this absolutely set in stone for the entire application? Can
 # we not have more than one plate size?
-DEFAULT_PLATE_HEIGHT=getenv('FG_DEFAULT_PLATE_HEIGHT',default=8)
-DEFAULT_PLATE_LENGTH=('FG_DEFAULT_PLATE_LENGTH',default=16)
+DEFAULT_PLATE_HEIGHT=getenv('FG_DEFAULT_PLATE_HEIGHT', default=8)
+DEFAULT_PLATE_LENGTH=('FG_DEFAULT_PLATE_LENGTH', default=16)
 
 # Permissions and Views
 
@@ -102,18 +99,18 @@ DEFAULT_PLATE_LENGTH=('FG_DEFAULT_PLATE_LENGTH',default=16)
 
 DATABASES = {
     'default': {
-        'ENGINE': getenv('FG_DB_ENGINE',default='django.db.backends.postgresql_psycopg2'),
-        'NAME': getenv('FG_DB_NAME',default='postgres'),
-        'USER': getenv('FG_DB_USER',default='postgres'),
-        'HOST': getenv('FG_DB_HOST',default='db'),
-        'PORT': getenv('FG_DB_PORT',default='5432'),
+        'ENGINE': getenv('FG_DB_ENGINE', default='django.db.backends.postgresql_psycopg2'),
+        'NAME': getenv('FG_DB_NAME', default='postgres'),
+        'USER': getenv('FG_DB_USER', default='postgres'),
+        'HOST': getenv('FG_DB_HOST', default='db'),
+        'PORT': getenv('FG_DB_PORT', default='5432'),
     }
 }
 
 # Rate Limits
 
-VIEW_RATE_LIMIT=getenv('FG_VIEW_RATE_LIMIT',default="50/1d")  # The rate limit for each view, django-ratelimit, "50 per day per ipaddress)
-VIEW_RATE_LIMIT_BLOCK=getenv('FG_VIEW_RATE_LIMIT_BLOCK',default=True) # Given that someone goes over, are they blocked for the period?
+VIEW_RATE_LIMIT=getenv('FG_VIEW_RATE_LIMIT', default="50/1d")  # The rate limit for each view, django-ratelimit, "50 per day per ipaddress)
+VIEW_RATE_LIMIT_BLOCK=getenv('FG_VIEW_RATE_LIMIT_BLOCK', default=True) # Given that someone goes over, are they blocked for the period?
 
 # Plugins
 # Add the name of a plugin under fg.plugins here to enable it
