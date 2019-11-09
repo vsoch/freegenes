38a82a5e26fa05ebfacd5c2c6aa1da5a1f676112
Vanessa Sochat <vsochat@stanford.edu>
2019-11-09 09:18:15 -0500

    adding available parts button at top (links to distributions) to close #125
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- fg/apps/base/context_processors.py
+++ fg/apps/base/context_processors.py
@@ -9,12 +9,34 @@ with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 '''
 
 from django.conf import settings
+from fg.apps.main.models import (
+    Part,
+    Distribution
+)
+
+def get_unique_parts():
+    '''we use this function to get parts associated with a distribution (meaning
+       that they can be requested. This is different from the total parts in
+       FreeGenes, which would be this query:
+
+       Part.objects.values('gene_id').distinct().count()
+    '''
+    parts = set()
+    for distribution in Distribution.objects.all():
+        for plateset in distribution.platesets.all():
+            # Each plate should be the same, so we look at the first
+            plate = plateset.plates.first()          
+            for well in plate.wells.all():
+                parts.add(well.sample_wells.first().part.gene_id)
+    return parts         
+
 
 def domain_processor(request):
     return {'domain': settings.DOMAIN_NAME,
             'NODE_URI': settings.NODE_URI,
             'NODE_NAME': settings.NODE_NAME,
-            'NODE_TWITTER': settings.NODE_TWITTER}
+            'NODE_TWITTER': settings.NODE_TWITTER,  # unique parts
+            'NODE_PARTS': len(get_unique_parts())}
 
 def help_processor(request):
     return {'HELP_CONTACT_EMAIL': settings.HELP_CONTACT_EMAIL,
